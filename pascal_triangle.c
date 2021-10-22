9a40e7fd8d3a04747b6fcf96b3e3302ca9fb20c8
SwaroopSPradhan <92518029+SwaroopSPradhan@users.noreply.github.com>
2021-10-22 22:40:21 +0530

    Changed variable names

--- pascal_triangle.c
+++ pascal_triangle.c
@@ -8,23 +8,22 @@
 #include <stdio.h>
 int main() 
 {
-   int rows, term = 1, space, i, j;
+   int rows, term = 1, i, j;
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
    for (i = 0; i < rows; i++)   //controls the no of rows
    {
-      for (sp= 1; sp<= rows - i; sp++)   //controls the no of spaces before printing the first charecter
+      for (int sp= 1; sp<= rows - i; sp++)   //controls the no of spaces before printing the first charecter
          printf("  ");
       for (j = 0; j <= i; j++)   //controls the digits in a row
      	 {
         	 if (j == 0)
-          	  coef = 1;
+          	   term = 1;
         	 else
-        	    term = term * (i - j + 1) / j;  //prints the digitsin a row
+        	    term = term * (i - j + 1) / j;  //prints the digits in a row
           printf("%4d", term);
      	 }
       printf("\n");
    }
    return 0;
 }
-
