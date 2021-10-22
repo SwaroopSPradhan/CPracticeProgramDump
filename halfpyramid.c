1127cbbbde337fb5412685e59ce07e4d9b8dad9c
SwaroopSPradhan <92518029+SwaroopSPradhan@users.noreply.github.com>
2021-10-22 22:50:10 +0530

    Output indentation fixed
    added:-  printf("\n") ;

--- halfpyramid.c
+++ halfpyramid.c
@@ -12,6 +12,7 @@ int main()
    int i, j, rows;
    printf("Enter the number of rows: ") ;
    scanf("%d", &rows) ;
+   printf("\n") ;
    for (i = 1; i <= rows; ++i)  //decides the number of rows in which the pattern is to be printed
    {
       for (j = 1; j <= i; ++j)  //decides the no of times * is to be printed in a row
